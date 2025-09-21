c6fbb21c22cc52bc5fa5c43d549b52d8dfa08ec9
Salvatore Zizzi <salvo.zizzi@gmail.com>
2025-09-21 15:45:59 +0200

    Prepare release 0.2.0: add multiclass support and fallback

--- threshopt/__init__.py
+++ threshopt/__init__.py
@@ -1,13 +1,12 @@
-from .core import optimize_threshold
+from .core import ThreshOpt, optimize_threshold
 from .cv import optimize_threshold_cv
 from .metrics import gmean_score, youden_j_stat, balanced_acc_score
 
 __all__ = [
+    "ThreshOpt",
     "optimize_threshold",
     "optimize_threshold_cv",
     "gmean_score",
     "youden_j_stat",
     "balanced_acc_score",
 ]
-
-
